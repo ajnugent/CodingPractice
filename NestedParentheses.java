594c26058de7db5cad233ace211617130d002de5
ajnugent <10779206+ajnugent@users.noreply.github.com>
2019-07-25 23:28:42 -0600

    Add files via upload

---
+++ NestedParentheses.java
@@ -0,0 +1,36 @@
+/*
+You are given a string that contains left and right parenthesis characters.
+Write code to determine whether the parentheses are correctly nested.
+For example, the strings "(())" and "()()" are correct but "(() ()" and ") (" are not.
+ */
+
+import java.util.Scanner;
+
+public class NestedParentheses {
+
+    public static boolean checkNesting(String s){
+        int count = 0;
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            if( ch == '('){
+                ++count;
+            }else if ( ch == ')'){
+                --count;
+                if (count < 0) return false;
+            }
+        }
+        return count == 0;
+    }
+
+    public static void main(String []args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter parentheses: ");
+
+        String s = in.nextLine();
+
+        checkNesting(s);
+    }
+
+}
