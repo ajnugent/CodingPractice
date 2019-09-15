263f878d4fea290da00f098a86e8aeb59a47725d
ajnugent <10779206+ajnugent@users.noreply.github.com>
2019-09-14 22:20:04 -0500

    Add files via upload

---
+++ IntMinMax.java
@@ -0,0 +1,12 @@
+public class IntMinMax {
+
+    public static void main(String[] args) {
+
+        /*System.out.printf("Max of int: %s  Min of int: %s%n", Integer.MAX_VALUE, Integer.MIN_VALUE);
+        System.out.printf("Max of long: %s  Min of long: %s%n", Long.MAX_VALUE, Long.MIN_VALUE);*/
+        System.out.printf("Max of float: %s  Min of float: %s%n", Float.MAX_VALUE, Float.MIN_VALUE);
+        System.out.printf("Max of double: %s  Min of double: %s%n", Double.MAX_VALUE, Double.MIN_VALUE);
+
+    }
+
+}
