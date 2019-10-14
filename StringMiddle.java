c715f38770aa90ba438fd4df107e8d6919b28126
ajnugent <jackieconcepts13@yahoo.com>
2019-10-13 21:13:46 -0500

    String Middle exercise

---
+++ StringMiddle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringMiddle {
+
+    public static void returnMiddle(String str){
+
+        if(str.length() == 3){
+            System.out.println("Result: " + str);
+        }else if(str.length() % 2 ==0){
+            System.out.println("String is of even length: INVALID");
+        }else{
+
+            int middle = str.length() / 2;
+
+            System.out.println("Result: " + str.substring(middle - 1, middle + 2));
+        }
+
+    }
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter an ODD length string: ");
+        String n = in.nextLine();
+        returnMiddle(n);
+
+    }
+}
